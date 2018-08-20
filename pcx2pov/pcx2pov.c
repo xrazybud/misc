66f6f70119e50461ae3fc0302204ead7ea1014ed
xrazybud <alex.bylund@gmail.com>
2018-08-20 12:19:24 -0600

    betterized

--- pcx2pov/pcx2pov.c
+++ pcx2pov/pcx2pov.c
@@ -10,17 +10,16 @@ int main(int argc, char *argv[]){
    al_init();
    al_init_image_addon();
 
-   povpic = al_load_bitmap("./cheshire-cat-icon-16.pcx");
-
+   povpic = al_load_bitmap("cheshire-cat-icon-16.pcx");
    if (povpic == NULL) {printf("ERROR\n"); return 0;}
 
    int picx = al_get_bitmap_width(povpic);
    int picy = al_get_bitmap_height(povpic);
 
-   printf("background {color rgb<1,1,1>}\n\n");
+   printf("#version 3.7\n\nbackground {color rgb<1,1,1>}\n");
    printf("global_settings { ambient_light rgb<2, 2, 2> }\n");
-   printf("light_source { <12, 12, 12> color rgb<1,1,1> }\n");
-   printf("camera {\northographic\nlocation <10,10,10>\nlook_at <0,0,0>\nangle 45\n}\n\n");
+   printf("light_source { <10, 0, 0> color rgb<1,1,1> }\n");
+   printf("camera {orthographic location <10,10,10> look_at <0,0,0> angle 45}\n\n");
 
    int x, y;
    float startx, starty, endx, endy;
@@ -29,15 +28,16 @@ int main(int argc, char *argv[]){
 
    for (x = 1; x < picx; x++) {
       for (y = 1; y < picy; y++) {
-         startx = -4+(0.5*(1*x));
+         startx = -(picx/2*0.5)+(0.5*(x));
          endx = startx + 0.5;
 
-         starty = -4+(0.5*(1*y));
+         starty = -(picy/2*0.5)+(0.5*(y));
          endy = starty + 0.5;
 
          currentcolor = al_get_pixel(povpic, x, y);
 
-         printf("box { <-0.5, %f, %f>, <0, %f, %f> texture {pigment {color rgb<%f,%f,%f>} } }\n", starty, startx, endy, endx, currentcolor.r, currentcolor.g, currentcolor.b);
+         printf("box { <-0.5,%f,%f>,<0,%f,%f> texture {pigment {color rgb<%f,%f,%f>} } }\n",
+                 starty, startx, endy, endx, currentcolor.r, currentcolor.g, currentcolor.b);
       }
       printf("\n");
    }
