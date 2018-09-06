14c7fbc1483381ebc8beb612c83f246e155428c1
xrazybud <alex.bylund@gmail.com>
2018-09-06 00:15:10 -0600

    should be done/refined

---
+++ live-wallpaper/README.md
@@ -0,0 +1,12 @@
+# live-wallpaper
+
+Makes a live wallpaper
+
+<p align="center">
+<img src="https://raw.githubusercontent.com/xrazybud/misc/master/live-wallpaper/screenshot.jpg" width=800>
+</p>
+
+## Notes
+- The wheel is a giant minute wheel, with the actual minute place shaded
+- Text is updated regularly with a console command
+- It's grepping/tailing logs now, but you could do 'watch -n 60 w' or whatever you want
--- live-wallpaper/live-dive.py
+++ live-wallpaper/live-dive.py
@@ -6,14 +6,16 @@ base = Image.open('coool2.png').convert('RGBA')
 wheel = Image.open('coool.png').convert('RGBA')
 
 txt = Image.new('RGBA', base.size, (255,255,255,0))
-fnt = ImageFont.truetype('Pillow/Tests/fonts/FreeMono.ttf', 18)
+#fnt = ImageFont.truetype('Pillow/Tests/fonts/FreeMono.ttf', 18)
+fnt = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf', 14)
 d = ImageDraw.Draw(txt)
 
-d.text((10,60), subprocess.run(['tail', '/home/alex/irclogs/Skynet/#twitter.log'], stdout=subprocess.PIPE).stdout.decode('utf-8'), font=fnt, fill=(255,255,255,255))
-d.text((10,300), subprocess.run(['tail', '/home/alex/irclogs/Skynet/#twitter2.log'], stdout=subprocess.PIPE).stdout.decode('utf-8'), font=fnt, fill=(255,255,255,255))
+d.text((60,60), subprocess.run('grep -v "danspena" /home/alex/irclogs/Skynet/#twitter.log | tail | cut -d " " -f2-', stdout=subprocess.PIPE, shell=True).stdout.decode('utf-8'), font=fnt, fill=(255,255,255,255))
+d.text((60,300), subprocess.run('tail /home/alex/irclogs/Skynet/#twitter2.log | cut -d " " -f2-', stdout=subprocess.PIPE, shell=True).stdout.decode('utf-8'), font=fnt, fill=(255,255,255,255))
+d.text((10,1016), subprocess.run('date', stdout=subprocess.PIPE, shell=True).stdout.decode('utf-8'), font=fnt, fill=(255,255,255,90))
 
 now = datetime.datetime.now()
-preout = Image.alpha_composite(base, wheel.rotate( (now.minute/66)*100) )
+preout = Image.alpha_composite(base, wheel.rotate( -(now.minute/0.1666) ))
 out = Image.alpha_composite(preout, txt)
 
 out.save("/home/alex/out.png")
